592511c106aea382f2bcfe7a3e0295106555154c
alex-kukhtin <alex.kukhtin@outlook.com>
2026-04-05 16:35:16 +0300

    update xaml

---
+++ A2v10.Xaml.Report/Barcode.cs
@@ -0,0 +1,21 @@
+﻿// Copyright © 2024-2026 Oleksandr Kukhtin. All rights reserved.
+
+using System;
+using System.Windows.Markup;
+
+namespace A2v10.Xaml.Report;
+
+public enum BarcodeType
+{
+    Ean13,
+    Ean8
+}
+
+[ContentProperty("Value")]
+public class Barcode : FlowElement
+{
+    public Object? Value { get; set; }
+    public Length? Width { get; set; }
+    public Int32 Height { get; set; }
+    public BarcodeType Type { get; set; }
+}
